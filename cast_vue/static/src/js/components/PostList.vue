686dcc973b79a79d8cd77e2ad24ecee088c00345
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-04-29 16:50:01 +0200

    load data from wagtail api for blog list page...

--- cast_vue/static/src/js/components/PostList.vue
+++ cast_vue/static/src/js/components/PostList.vue
@@ -1,16 +1,45 @@
-<script setup lang="ts">
-</script>
-
 <template>
-    <h1>Some Title</h1>
+  <div>
+      <p v-if="isLoading">Loading data...</p>
+      <div v-else>
+        <h1>{{ data.title }}</h1>
+        {{ data }}
+      </div>
+  </div>
 </template>
- 
+
 <script lang="ts">
+import { ref, onMounted } from 'vue';
+
 export default {
-  data() {
-    return {
-      message: "Hi from App.vue",
-    };
-  },
+    setup() {
+        const isLoading = ref(true);
+        const data = ref(null);
+
+        const fetchData = async () => {
+            try {
+                const apiUrlElement = document.getElementById("blog-api-url");
+                if (apiUrlElement === null || apiUrlElement.textContent === null) {
+                    throw new Error(`Could not find element with id "blog-api-url"`);
+                }
+                const pageUrl = JSON.parse(apiUrlElement.textContent);
+                const response = await fetch(pageUrl);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                data.value = await response.json();
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                isLoading.value = false;
+            }
+        };
+
+        onMounted(fetchData);
+        return {
+            isLoading,
+            data
+        };
+    },
 };
 </script>
