29bacf3f5bac81626413693ab004b336a732316c
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-04-30 09:06:49 +0200

    added pagination and ordering

--- cast_vue/static/src/js/components/LoadPostList.vue
+++ cast_vue/static/src/js/components/LoadPostList.vue
@@ -2,6 +2,12 @@
   <div>
       <p v-if="isLoading">Loading data...</p>
       <div v-else>
+        <br />
+        <div class="pagination">
+            <button @click="changePage(-1)" :disabled="currentPage <= 1">&laquo; Prev</button>
+            <span>Page {{ currentPage }} of {{ totalPages }}</span>
+            <button @click="changePage(1)" :disabled="currentPage >= totalPages">Next &raquo;</button>
+        </div>
         <post-list :blog="blog" :posts="postsFromApi"/>
       </div>
   </div>
@@ -12,13 +18,7 @@ import { ref, onMounted } from 'vue';
 import PostList from './PostList.vue'
 import { PostsFromApi } from './types';
 
-
-async function fetchElementData(elementId: string) {
-    const element = document.getElementById(elementId);
-    if (element === null || element.textContent === null) {
-        throw new Error(`Could not find element with id "${elementId}"`);
-    }
-    const url = JSON.parse(element.textContent);
+async function fetchPostsFromApi(url: string) {
     const response = await fetch(url);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -26,9 +26,60 @@ async function fetchElementData(elementId: string) {
     return await response.json();
 }
 
+
+async function fetchElementData(elementId: string) {
+    const element = document.getElementById(elementId);
+    if (element === null || element.textContent === null) {
+        throw new Error(`Could not find element with id "${elementId}"`);
+    }
+    const url = JSON.parse(element.textContent);
+    console.log(`Fetching data from ${url}`);
+    console.log("elementId: ", elementId);
+    if (elementId === "blog-post-list-api-url") {
+        const postsUrl = url + "&limit=5&offset=0";
+        return await fetchPostsFromApi(postsUrl);
+    } else {
+        return await fetchPostsFromApi(url);
+    }
+}
+
 export default {
     components: {
         PostList
+    },
+    data() {
+        return {
+            currentPage: 1,
+            itemsPerPage: 5,
+        };
+    },
+    computed: {
+        totalPages() {
+            if (!this.isLoading && this.postsFromApi.items !== undefined) {
+                return Math.ceil(this.postsFromApi.meta.total_count / this.itemsPerPage);
+            } else {
+                return 0;
+            }
+        },
+        paginatedPosts() {
+            const start = (this.currentPage - 1) * this.itemsPerPage;
+            const end = start + this.itemsPerPage;
+            return this.postsFromApi.items.slice(start, end);
+        },
+    },
+    methods: {
+        async changePage(delta: number) {
+            this.currentPage += delta;
+            const url = `../api/wagtail/pages?child_of=4&order=-first_published_at&limit=5&offset=${this.currentPage}`;
+            console.log("change page: ", this.currentPage);
+            console.log("url: ", url);
+            try {
+                this.postsFromApi = await fetchPostsFromApi(url);
+                console.log("postsFromApi: ", this.postsFromApi)
+            } catch (error) {
+                console.error('Error fetching data from API: ', error);
+            }
+        },
     },
     setup() {
         const isLoading = ref(true);
@@ -57,7 +108,7 @@ export default {
         return {
             isLoading,
             blog,
-            postsFromApi
+            postsFromApi,
         };
     },
 };
