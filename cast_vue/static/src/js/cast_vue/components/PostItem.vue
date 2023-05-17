39e99c6bfe1f37d973034ee11de8f13fbc389675
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-05-17 07:16:15 +0200

    added values for time and author

--- cast_vue/static/src/js/cast_vue/components/PostItem.vue
+++ cast_vue/static/src/js/cast_vue/components/PostItem.vue
@@ -1,44 +1,64 @@
 <template>
-    <div class="post-item">
-      <h2>{{ post.title }}</h2>
-      <div v-if="detail">
-        <p>{{ visibleDate }}</p>
-      </div>
-      <div v-else>
-        visible date detail
-        <p>
-          <router-link :to="{ name: 'PostDetail', params: {slug: post.meta.slug}}">
-            {{ visibleDate }}
-          </router-link>
-        </p>
-      </div>
-      <div v-html="post.html_overview"></div>
+  <div class="post-item">
+    <h2>{{ post.title }}</h2>
+    <div v-if="detail">
+      <p>
+        <time :date-time="visibleDateTimeStr">{{ visibleDate }}</time
+        >, by
+        <span class="author">{{ author }}</span>
+      </p>
     </div>
-  </template>
+    <div v-else>
+      visible date detail
+      <p>
+        <router-link :to="{ name: 'PostDetail', params: { slug: post.meta.slug } }">
+          <time :date-time="visibleDateTimeStr">{{ visibleDate }}</time> </router-link
+        >, by
+        <span class="author">{{ author }}</span>
+      </p>
+    </div>
+    <div v-html="post.html_overview"></div>
+  </div>
+</template>
 
-  <script lang="ts">
-  import { getTexContentFromElement } from './domHelpers';
-  import { Post } from './types';
+<script lang="ts">
+import { getTexContentFromElement } from "./domHelpers";
+import { Post } from "./types";
 
-
-  export default {
-    name: "PostItem",
-    props: {
-      post: {
-        type: Object as () => Post,
-        required: true,
-      },
-      detail: {
-        type: Boolean,
-        default: false,
-      },
+export default {
+  name: "PostItem",
+  props: {
+    post: {
+      type: Object as () => Post,
+      required: true,
     },
-    computed: {
-      visibleDate(): string {
-        const visibleDateStr = getTexContentFromElement("vue-article-date", this.post.html_detail);
-        console.log("visibleDateStr: ", visibleDateStr);
-        return visibleDateStr;
-      },
+    detail: {
+      type: Boolean,
+      default: false,
     },
-  };
-  </script>
+  },
+  computed: {
+    visibleDate(): string {
+      const visibleDateStr = getTexContentFromElement(
+        "vue-article-date",
+        this.post.html_detail
+      );
+      return visibleDateStr;
+    },
+    visibleDateTimeStr(): string {
+      const visibleDateTimeStr = getTexContentFromElement(
+        "vue-article-datetime",
+        this.post.html_detail
+      );
+      return visibleDateTimeStr;
+    },
+    author(): string {
+      const author = getTexContentFromElement(
+        "vue-article-author",
+        this.post.html_detail
+      );
+      return author;
+    },
+  },
+};
+</script>
